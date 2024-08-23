5d0f18e2e34cfd38894e562cfa8e7ecc0a3811e8
BirkurwarJay <birkurwarjay01@gmail.com>
2024-08-23 19:17:55 +0530

    added gsap scroll animation

--- ss.js
+++ ss.js
@@ -1,15 +1,31 @@
-let tl = gsap.timeline();
-
 let projects = document.querySelector(".projects");
+let img = document.querySelectorAll(".projects img");
 
-gsap.to(projects, {
-    transform: "translateX(-30%)",
+function entryAnimation() {
+    let tl = gsap.timeline();
+
+    tl.from(".nav_logo, .navbar-nav li, .Qbtn",{
+        opacity: 0,
+        scale: 0,
+        stagger: .1,
+        delay: 1
+    })
+    tl.from(".img-1", {
+        scale: 0.4,
+        opacity: 0,
+        duration: .5
+    })
+}
+
+entryAnimation();
+
+gsap.to(img, {
+    transform: "translateX(-180%)",
     scrollTrigger: {
-        trigger: "#expro",
+        trigger: projects,
         scroller: "body",
-        start: "top 40%",
-        end: "top 100%",
-        scrub: 5,
-        pin: true
-    },
+        start: "top 50%",
+        end: "top 0%",
+        scrub: 2,
+    }
 })
